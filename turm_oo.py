3740011ebcc15be23c2f2e0feef72c3bb06a1ad9
Michael Krisper <michael.krisper@gmail.com>
2014-11-03 00:38:30 +0100

    added getitem, repr

--- turm_oo.py
+++ turm_oo.py
@@ -1,9 +1,13 @@
+#!/usr/bin/python3
+# coding=utf-8
+
 import sys
 
 
 class Tower():
     def __init__(self, number):
         self.number = number
+        self.__items = None
 
     def __iter__(self):
         def it(nr):
@@ -15,14 +19,27 @@ class Tower():
                 yield (nr, " / ", fac)
                 nr //= fac
             yield (nr, "", "")
+
         return it(self.number)
 
+    def __getitem__(self, item):
+        if not self.__items:
+            self.__items = list(iter(self))
+        return self.__items[item]
+
+    @property
+    def max_nr(self):
+        return self.number * 362880
 
     def __str__(self):
-        lines = []
-        pad = len(str(self.number * 362880)) + 1
-        return "\n".join("{number:{pad}}{op}{fac}".format(pad=pad, op=op, number=nr, fac=fac) for nr, op, fac in iter(self))
+        pad = len(str(self.max_nr)) + 1
+        return "\n".join(
+            "{number:{pad}}{op}{fac}".format(pad=pad, op=op, number=nr, fac=fac) for nr, op, fac in iter(self))
+
+    def __repr__(self):
+        return "Tower({})".format(self.number)
 
 
 number = int(sys.argv[1])
-print(Tower(number))
+t = Tower(number)
+print(t)
