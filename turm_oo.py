18e606de2fa20b1b04df219fc0a7589c2b642d75
Michael Krisper <michael.krisper@gmail.com>
2014-11-03 00:09:12 +0100

    oo with str over iterator

--- turm_oo.py
+++ turm_oo.py
@@ -5,23 +5,24 @@ class Tower():
     def __init__(self, number):
         self.number = number
 
+    def __iter__(self):
+        def it(nr):
+            for fac in range(2, 10):
+                yield (nr, " * ", fac)
+                nr *= fac
+
+            for fac in range(2, 10):
+                yield (nr, " / ", fac)
+                nr //= fac
+            yield (nr, "", "")
+        return it(self.number)
+
+
     def __str__(self):
-        nr = number
         lines = []
-        pad = len(str(nr * 362880)) + 1
-
-        for multiplikator in range(2, 10):
-            lines.append("{number:{pad}} * {fac}".format(pad=pad, number=nr, fac=multiplikator))
-            nr *= multiplikator
-
-        for divident in range(2, 10):
-            lines.append("{number:{pad}} / {fac}".format(pad=pad, number=nr, fac=divident))
-            nr //= divident
-
-        lines.append("{number:{pad}}".format(pad=pad, number=number))
-        return "\n".join(lines)
+        pad = len(str(self.number * 362880)) + 1
+        return "\n".join("{number:{pad}}{op}{fac}".format(pad=pad, op=op, number=nr, fac=fac) for nr, op, fac in iter(self))
 
 
 number = int(sys.argv[1])
 print(Tower(number))
-
